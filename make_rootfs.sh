1d2f3ce79e0af079dcedc9df20fa13b48e4c0d0f
John Reed <johnr@loogatee.com>
2020-05-20 16:45:41 -0700

    refinement with make_rootfs.sh and make_sdcard.sh

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -x
 
+HOSTNAME="imxyz6"
+
 LINUX_DIR=/home/johnr/linux-imx6
-SDCARD=/media/johnr/a70226db-d7db-4c1c-82d6-bbef8c41f203
 UBUNTU_NAME="ubuntu-base-14.04.5-base-armhf.tar.gz"
 
 
@@ -43,74 +44,90 @@ rm -f ./$UBUNTU_NAME
 #
 #   Modifies the stock Ubuntu Core Release with these files
 #
-#sudo rsync -avD ../Nitro_UbuntuBase/rootfs/* .
+sudo rsync -avD Custom_Files/* rootfs
+
+#
+#   serial-console needs to be executable
+#
+sudo chmod 755 rootfs/bin/serial-console
 
 
 #
 #    An example of a simple addition to an existing file
 #    
-#sudo sed -i "\$aalias dir='ls -CF'" ./root/.bashrc
+sudo sed -i "\$aalias dir='ls -CF'" rootfs/root/.bashrc
 
 #
 #   Removes passwd from root
 #
-#sudo sed -i "s/root:x:0/root::0/g" ./etc/passwd
+sudo sed -i "s/root:x:0/root::0/g" rootfs/etc/passwd
 
+#
+#   Overwrites the default hostname
+#
+sudo /bin/bash -c "echo $HOSTNAME >   rootfs/etc/hostname"
+
+#
+#   Creates /etc/resolv.conf
+#
+sudo rm -f                                          rootfs/etc/resolv.conf
+sudo /bin/bash -c "echo nameserver 8.8.8.8 >        rootfs/etc/resolv.conf"
+sudo ln -s /var/run/resolvconf/resolv.conf          rootfs/etc/resolv.conf
 
 #
 #   Copy kernel images and device tree .dtb to local rootfs
 #
-#sudo cp $LINUX_DIR/vmlinux                                      ./boot
-#sudo cp $LINUX_DIR/arch/arm/boot/uImage                         ./boot
-#sudo cp $LINUX_DIR/arch/arm/boot/zImage                         ./boot
-#sudo cp $LINUX_DIR/arch/arm/boot/dts/imx6q-sabrelite-nolcd.dtb  ./boot/imx6q-sabrelite.dtb
+sudo cp $LINUX_DIR/vmlinux                                      rootfs/boot
+sudo cp $LINUX_DIR/arch/arm/boot/uImage                         rootfs/boot
+sudo cp $LINUX_DIR/arch/arm/boot/zImage                         rootfs/boot
+sudo cp $LINUX_DIR/arch/arm/boot/dts/imx6q-sabrelite-nolcd.dtb  rootfs/boot/imx6q-sabrelite.dtb
 
 
 #
 #   Prepare for module build by creating a destination directory
 #
-#sudo rm -rf /tmp/tmpXX
-#mkdir  /tmp/tmpXX
+sudo rm -rf /tmp/tmpXX
+mkdir  /tmp/tmpXX
 
 
 #
 #   Saves the current working directory, so we can return here again later
 #
-#pushd .
+pushd .
 
 
 #
 #   Make the modules
 #
-#cd $LINUX_DIR; export ARCH=arm; make INSTALL_MOD_PATH=/tmp/tmpXX modules_install
+cd $LINUX_DIR; export ARCH=arm; make INSTALL_MOD_PATH=/tmp/tmpXX modules_install
 
 
 #
 #   Get rid of the symbolic links
 #
-#find /tmp/tmpXX/lib/modules -type l -exec rm -f {} \;
+find /tmp/tmpXX/lib/modules -type l -exec rm -f {} \;
 
 
 #
 #   Now return to the directory location that we saved earlier
 #
-#popd
+popd
 
 
 #
 #   Copy the modules to the local rootfs
 #
-#sudo rsync -avD /tmp/tmpXX/lib/modules/* ./lib/modules/
+sudo rsync -avD /tmp/tmpXX/lib/modules/* rootfs/lib/modules/
 
 
-#
-#   Now finish up by copying the rootfs to disk
-#
-#sudo rsync -avD * $SDCARD
 
 
-#
-#   Waits until all the disk data is written
-#
-sync;sync;
+
+
+
+
+
+
+
+
 
---
+++ make_sdcard.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+SDCARD=/media/johnr/a70226db-d7db-4c1c-82d6-bbef8c41f203
+
+
+#
+#   Now finish up by copying the rootfs to disk
+#
+sudo rsync -avD rootfs/* $SDCARD
+
+
+#
+#   Waits until all the disk data is written
+#
+sync;sync;
+
