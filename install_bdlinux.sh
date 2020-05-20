f0bce3251361213097f4f80131fd42913fe764be
John Reed <johnr@loogatee.com>
2020-05-20 15:33:53 -0700

    interim: re-arranging and updating

--- install_bdlinux.sh
+++ install_bdlinux.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 
+LINUX_DIR=/home/johnr/linux-imx6
+SDCARD=/media/johnr/a70226db-d7db-4c1c-82d6-bbef8c41f203
+UBUNTU_NAME="ubuntu-base-14.04.5-base-armhf.tar.gz"
 
-LINUX_DIR=/opt/linux-imx6
-SDCARD=/media/jreed/6e120ed0-30d4-48a7-b6a0-df4fd71943d4
 
 
 #
@@ -11,96 +12,101 @@ SDCARD=/media/jreed/6e120ed0-30d4-48a7-b6a0-df4fd71943d4
 #     - All files owned by root
 #     - All files are root group except for /etc/shadow
 #
-sudo find rootfs -exec chown root {} \; -exec chgrp root {} \;
-sudo chgrp shadow rootfs/etc/shadow
+sudo find Custom_Files -exec chown root {} \; -exec chgrp root {} \;
+sudo chgrp shadow Custom_Files/etc/shadow
+
+
+sudo rm -rf rootfs
+mkdir rootfs
 
 
 #
 #   use wget to grab the ubuntu core release
 #
-cd ..; wget http://cdimage.ubuntu.com/ubuntu-core/releases/14.04.1/release/ubuntu-core-14.04.1-core-armhf.tar.gz
+#wget http://cdimage.ubuntu.com/ubuntu-base/releases/14.04.5/release/$UBUNTU_NAME
 
-
-#
-#   Ubuntu Core files will go into the rootfs directory
-#
-mkdir -p UbuntuCore_rootfs; cd UbuntuCore_rootfs
+cp ~/Downloads/$UBUNTU_NAME .
 
 
 #
 #   un-zip and un-tar the Ubuntu_Core files
 #
-sudo tar --numeric-owner -xzvf ../ubuntu-core-14.04.1-core-armhf.tar.gz
+cd rootfs; sudo tar --numeric-owner -xzvf ../$UBUNTU_NAME; cd ..
 
 
 #
 #   The .tar.gz file is no longer needed
 #
-rm -f ../ubuntu-core-14.04.1-core-armhf.tar.gz
+rm -f ./$UBUNTU_NAME
 
 
 #
 #   Modifies the stock Ubuntu Core Release with these files
 #
-sudo rsync -avD ../UbuntuCore_CustomFiles_BD/rootfs/* .
+#sudo rsync -avD ../Nitro_UbuntuBase/rootfs/* .
 
 
 #
 #    An example of a simple addition to an existing file
 #    
-sudo sed -i "\$aalias dir='ls -CF'" ./root/.bashrc
+#sudo sed -i "\$aalias dir='ls -CF'" ./root/.bashrc
+
+#
+#   Removes passwd from root
+#
+#sudo sed -i "s/root:x:0/root::0/g" ./etc/passwd
 
 
 #
 #   Copy kernel images and device tree .dtb to local rootfs
 #
-sudo cp $LINUX_DIR/vmlinux                                 ./boot
-sudo cp $LINUX_DIR/arch/arm/boot/uImage                    ./boot
-sudo cp $LINUX_DIR/arch/arm/boot/zImage                    ./boot
-sudo cp $LINUX_DIR/arch/arm/boot/dts/imx6q-nitrogen6x.dtb  ./boot
+#sudo cp $LINUX_DIR/vmlinux                                      ./boot
+#sudo cp $LINUX_DIR/arch/arm/boot/uImage                         ./boot
+#sudo cp $LINUX_DIR/arch/arm/boot/zImage                         ./boot
+#sudo cp $LINUX_DIR/arch/arm/boot/dts/imx6q-sabrelite-nolcd.dtb  ./boot/imx6q-sabrelite.dtb
 
 
 #
 #   Prepare for module build by creating a destination directory
 #
-sudo rm -rf /tmp/tmpXX
-mkdir  /tmp/tmpXX
+#sudo rm -rf /tmp/tmpXX
+#mkdir  /tmp/tmpXX
 
 
 #
 #   Saves the current working directory, so we can return here again later
 #
-pushd .
+#pushd .
 
 
 #
 #   Make the modules
 #
-cd $LINUX_DIR; make INSTALL_MOD_PATH=/tmp/tmpXX modules_install
+#cd $LINUX_DIR; export ARCH=arm; make INSTALL_MOD_PATH=/tmp/tmpXX modules_install
 
 
 #
 #   Get rid of the symbolic links
 #
-find /tmp/tmpXX/lib/modules -type l -exec rm -f {} \;
+#find /tmp/tmpXX/lib/modules -type l -exec rm -f {} \;
 
 
 #
 #   Now return to the directory location that we saved earlier
 #
-popd
+#popd
 
 
 #
 #   Copy the modules to the local rootfs
 #
-sudo rsync -avD /tmp/tmpXX/lib/modules/* ./lib/modules/
+#sudo rsync -avD /tmp/tmpXX/lib/modules/* ./lib/modules/
 
 
 #
 #   Now finish up by copying the rootfs to disk
 #
-sudo rsync -avD * $SDCARD
+#sudo rsync -avD * $SDCARD
 
 
 #
